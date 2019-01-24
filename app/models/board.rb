99b1b9ca2c65e35699d73b439e54e4a36bb5d21a
sevasu2 <babagame7@gmail.com>
2019-01-24 14:06:00 +0000

    バリデーション追加

---
+++ app/models/board.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  name       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Board < ApplicationRecord
+	has_many :comments
+	validates :name, presence: true, length: { maximum: 10}
+	validates :title, presence: true, length: { maximum: 30}
+	validates :body, presence: true, length: { maximum: 1000}
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  comment    :text             not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  voard_id   :integer
+#
+# Indexes
+#
+#  index_comments_on_voard_id  (voard_id)
+#
+
+class Comment < ApplicationRecord
+  belongs_to :voard
+end
